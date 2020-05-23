1599585e000ff4bb8bd5944d6ae63ef53d0b3db0
siddharth padhi <padhisiddharth31@gmail.com>
2020-05-24 01:12:29 +0530

    'first'

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import REsume1 from './resumes/resume1'
+
+export default function App(){
+  return(
+      <REsume1/>
+  )
+}
---
+++ src/resumes/resume1/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import './styles.css'
+export default function Resume(){
+    return(
+        <div className='container'>
+            <div className='container3'>
+
+            </div>
+             <div className='profile'>
+                 <div className='info'>
+                      Siddharth Padhi
+                 </div>
+                 
+
+             </div>
+           <div className='container2'>
+               <div className='about'>
+                   about me
+               </div>
+               <div className='second'>
+                   <div style={{paddingLeft:'20px',color:'#f9a039'}}>
+                       22 
+                   </div>
+                   <div style={{paddingLeft:'20px',color:'#f9a039'}}>
+                       Lviv 
+                   </div>
+                   <div style={{paddingLeft:'20px',color:'#f9a039'}}>
+                       Freelance 
+                   </div>
+
+               </div>
+               <div className='eas'>
+               Prolific, full stack web developer with a passion for metrics and beating former "best-yets." Prototyped 25 new product features per year for Flexor, Inc. Decreased rework by 22% and costs by 15%. Consistently receive high user experience scores for all web development projects, including a 55% increase for Flexor, Inc. Passionate about building world class web applications. One of my sites received a 2015 Webby for Best Navigation and Structure.
+
+
+               </div>
+               <hr>
+               </hr>
+               <div>
+
+               </div>
+                 
+           </div>
+        </div>
+    )
+}
